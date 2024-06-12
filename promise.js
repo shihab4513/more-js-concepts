0db69d607c42997477f9d199a0e2ee4df49b25b0
shihab4513 <shihab4513@gmail.com>
2024-06-12 16:16:43 +0600

    promise.js and html added . Here promise function discussed . Its a very important technical topic.

---
+++ promise.js
@@ -0,0 +1,16 @@
+const myLoader = () => {
+    return new Promise((resolve, reject) => {
+        const success = Math.random();
+        if (success <= 0.5) {
+            resolve(success);
+        }
+        else {
+            reject(success);
+        }
+    })
+}
+
+
+myLoader()
+    .then(data => console.log('resolved data', data))
+    .catch(error => console.log('rejected with value ', error));
